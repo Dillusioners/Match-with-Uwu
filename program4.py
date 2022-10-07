724a8d442c138dbc9d226147cad0dd5631e6aa35
LeeTuah <77256247+LeeTuah@users.noreply.github.com>
2022-10-07 22:40:42 +0530

    Create program4.py

---
+++ program4.py
@@ -0,0 +1,28 @@
+from itertools import permutations 
+
+num_str = ""
+i = 1
+
+#Taking the 5 number inputs
+while i <= 5:
+    try:
+        num = int(input(">> Enter term number {} :-> ".format(i)))
+    #Trying and excepting the ValueError for aplhabetical inputs
+    except ValueError:
+        print("Please input only real number literals :(")
+    else:
+        num_str += str(num)
+        i += 1
+
+#Getting the permutation tuples and storing them in the list perm_list
+perm_list = list(permutations(num_str))
+
+#iterating through perm_list[i][j] to print all the numbers
+for i in range(len(perm_list)):
+    new_str = ""
+    for j in range(len(perm_list[i])):
+        new_str += perm_list[i][j]
+    
+    print(new_str)
+
+print("Program has ended")
